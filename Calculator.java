9e9108907d20d836b103edf5e75f4c96cfca3068
Sebastian <sabys@hotmail.com>
2026-08-06 20:53:01 -0400

    Add main method to Calculator to demo sample calculations

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,16 @@
 public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("add(2, 3) = " + add(2, 3));
+        System.out.println("subtract(3, 2) = " + subtract(3, 2));
+        System.out.println("multiply(2, 3) = " + multiply(2, 3));
+        System.out.println("divide(6, 3) = " + divide(6, 3));
+        try {
+            divide(1, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("divide(1, 0) threw ArithmeticException: " + e.getMessage());
+        }
+    }
+
     public static double add(double a, double b) {
         return a + b;
     }
